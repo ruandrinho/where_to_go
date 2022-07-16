a2094a569983ba3f446269f0e93b2761bb5f2521
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-07-16 16:37:32 +0500

    Move data from template to view

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -23,6 +23,7 @@ from where_to_go import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main_page)
+    path('', views.main_page),
+    path('places/<int:place_id>/', views.place_detail_view)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +\
     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,6 +1,43 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404
+from places.models import Place
 
 
 def main_page(request):
-    return render(request, 'index.html')
+    places_for_page = {
+        'type': 'FeatureCollection',
+        'features': []
+    }
+    for place in Place.objects.all():
+        place_for_page = {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.longitude, place.latitude]
+            },
+            'properties': {
+                'title': place.title,
+                'placeId': place.id,
+                'detailsUrl': f'/places/{place.id}/'
+            }
+        }
+        places_for_page['features'].append(place_for_page)
+    context = {'places_for_page': places_for_page}
+    return render(request, 'index.html', context=context)
+
+
+def place_detail_view(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+    context = {
+        'title': place.title,
+        'imgs': [photo.photo.url for photo in
+                 place.photos.all().order_by('priority')],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lng': place.longitude,
+            'lat': place.latitude
+        }
+    }
+    return JsonResponse(context, safe=False, 
+                        json_dumps_params={'ensure_ascii': False, 'indent': 4})
