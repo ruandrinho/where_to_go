c04a5a74b0c7bc20bf9e2e717ca6e6742c8a93a2
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-07-16 18:52:41 +0500

    Add image previews to Place admin page

--- places/admin.py
+++ places/admin.py
@@ -1,13 +1,29 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from places.models import Place, PlacePhoto
+
 
 # Register your models here.
 class PhotoInline(admin.TabularInline):
     model = PlacePhoto
     extra = 1
-    
+    readonly_fields = ['photo_preview', ]
+    fields = ('photo', 'photo_preview', 'priority')
+
+    def photo_preview(self, obj):
+        height = min(obj.photo.height, 200)
+        width = int(obj.photo.width * height / obj.photo.height)
+        return format_html(
+            '<img src="{}" width="{}" height="{}">',
+            obj.photo.url, width, height
+        )
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    inlines = [PhotoInline,]
+    inlines = [PhotoInline, ]
 
-admin.site.register(PlacePhoto)
+
+@admin.register(PlacePhoto)
+class PlacePhotoAdmin(admin.ModelAdmin):
+    pass
