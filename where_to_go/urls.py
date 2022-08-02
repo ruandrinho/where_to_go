e4efac3fb41096e08623c4316e85ae5bcd4e3c6d
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-08-02 13:48:46 +0500

    Fix errors with static and media

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from where_to_go import views
-
+from where_to_go import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +26,7 @@ urlpatterns = [
          name='single_place'),
     path('tinymce/', include('tinymce.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
